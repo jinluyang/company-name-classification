ffa65391d17d5d4df9ed4e288b49bd4be8d054db
xiaoyang <1055057679@qq.com>
2016-08-05 16:52:18 +0800

    3

--- load_model.py
+++ load_model.py
@@ -25,6 +25,43 @@ def load_param():
     return param_blocks
 
 
+def setup_cnn_model(ctx, batch_size, sentence_size, num_embed, vocab_size,
+        dropout=0.5, initializer=mx.initializer.Uniform(0.1), with_embedding=True):
+
+    cnn=mx.sym.load('./symbol')
+    arg_names = cnn.list_arguments()
+
+    input_shapes = {}
+    if with_embedding:
+        input_shapes['data'] = (batch_size, 1, sentence_size, num_embed)
+    else:
+        input_shapes['data'] = (batch_size, sentence_size)
+
+    arg_shape, out_shape, aux_shape = cnn.infer_shape(**input_shapes)	#第二个看不懂
+    arg_arrays = [mx.nd.zeros(s, ctx) for s in arg_shape]
+    args_grad = {}
+    for shape, name in zip(arg_shape, arg_names):
+        if name in ['softmax_label', 'data']: # input, output
+            continue
+        args_grad[name] = mx.nd.zeros(shape, ctx)
+
+    cnn_exec = cnn.bind(ctx=ctx, args=arg_arrays, args_grad=args_grad, grad_req='add')
+
+    param_blocks = []
+    arg_dict = dict(zip(arg_names, cnn_exec.arg_arrays))
+    for i, name in enumerate(arg_names):
+        if name in ['softmax_label', 'data']: # input, output
+            continue
+        initializer(name, arg_dict[name])
+
+        param_blocks.append( (i, arg_dict[name], args_grad[name], name) )
+
+    out_dict = dict(zip(cnn.list_outputs(), cnn_exec.outputs))
+
+    data = cnn_exec.arg_dict['data']
+    label = cnn_exec.arg_dict['softmax_label']
+
+    return CNNModel(cnn_exec=cnn_exec, symbol=cnn, data=data, label=label, param_blocks=param_blocks)
 
 pkl_file = open('vocab.pkl', 'rb')
 vocab  = pickle.load(pkl_file)
@@ -50,12 +87,13 @@ num_embed = 50
 batch_size = 100
 sentence_size = sentence_test.shape[1]
 
+cnn_model = setup_cnn_model(mx.cpu(), batch_size, sentence_size, num_embed, vocab_size, dropout=0.5, with_embedding=False) 
 
-#l=[]
-#for begin in range(0,sentence_test.shape[0] ,100):
-#    batch=sentence_test[begin:begin+100]
-#    if batch.shape[0]!=100:
-#        break
-#    model_loaded.data[:]=batch
-#    m.cnn_exec.forward(is_train=False)
-#    l.extend(np.argmax(m.cnn_exec.outputs[0].asnumpy(), axis=1))
+l=[]
+for begin in range(0,sentence_test.shape[0] ,100):
+    batch=sentence_test[begin:begin+100]
+    if batch.shape[0]!=100:
+        break
+    cnn_model.data[:]=batch
+    cnn_model.cnn_exec.forward(is_train=False)
+    l.extend(np.argmax(cnn_model.cnn_exec.outputs[0].asnumpy(), axis=1))
